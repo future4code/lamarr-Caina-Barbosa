0d8f63dd5ea8aabb4c408ac75be9917dfc479cdc
Cainã Perotti Barbosa <caiperotti@gmail.com>
2022-05-10 18:44:51 -0300

    Exercício 12

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -129,7 +129,10 @@ function retornaChamadaDeFilme(filme) {
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+    return {
+        ...pessoa,
+        nome : "ANÔNIMO"
+    }
 }
 
 // EXERCÍCIO 13A
