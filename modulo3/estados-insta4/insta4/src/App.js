d25015a90cd69dc6323116dbb3775e060bff63be
Cainã Perotti Barbosa <caiperotti@gmail.com>
2022-07-07 20:54:08 -0300

    Adiciona formulário de envio de post

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './components/Post/Post';
 import './style.css'
 
 
+
 function App() {
-const arrayPost = [
+  
+  const [arrayPost, setArrayPost] = useState([
     {
     nomeUsuario: "paulinha",
     fotoUsuario: "https://picsum.photos/50/50",
@@ -20,22 +22,53 @@ const arrayPost = [
     fotoUsuario:'https://pbs.twimg.com/profile_images/1518543225933512705/T4r0T3SE_400x400.jpg',
     fotoPost:'https://i1.sndcdn.com/artworks-000232693634-bambr5-t500x500.jpg'
     }
-]
+])
+  const [inputNome, setInputNome] = useState("");
+  const [inputFotoUser, setFotoUser] = useState("");
+  const [inputFoto, setFoto] = useState("");
+
+
+  const handleInputNome = (e) => {
+    setInputNome(e.target.value)  
+  }
+
+  const handleInputFotoUser = (e) => {
+    setFotoUser(e.target.value)  
+  }
+
+  const handleInputFoto = (e) => {
+    setFoto(e.target.value)  
+  }
+
+  const aoEnviarFoto = () => {
+    setArrayPost([...arrayPost, {nomeUsuario: inputNome, fotoUsuario: inputFotoUser, fotoPost: inputFoto}])
+    setFoto("")
+    setFotoUser("")
+    setInputNome("")
+  }
+
   const dadosPostMapeados = arrayPost.map((valor, index) => { 
-  return <Post 
-  nomeUsuario = {valor.nomeUsuario}
-  fotoUsuario = {valor.fotoUsuario}
-  fotoPost = {valor.fotoPost}
-  key = {index}
-  />
-  })
+    return <Post 
+    nomeUsuario = {valor.nomeUsuario}
+    fotoUsuario = {valor.fotoUsuario}
+    fotoPost = {valor.fotoPost}
+    key = {index}
+    />
+    })
   
-return (
-  <div className='MainContainer'>
-    {dadosPostMapeados}
-    {console.log(arrayPost)}
-  </div>
-)
+  return (
+    <div className='MainContainer'>
+      <label>Nome do Usuário</label>
+      <input value = {inputNome} onChange = {handleInputNome}></input>
+      <label>Foto do Usuário</label>
+      <input value = {inputFotoUser} onChange = {handleInputFotoUser}></input>
+      <label>Foto</label>
+      <input value = {inputFoto} onChange = {handleInputFoto}></input>
+      <button onClick = {aoEnviarFoto} >Enviar Post</button>
+      {dadosPostMapeados}
+      {console.log(arrayPost)}
+    </div>
+  )
 
 }
 
